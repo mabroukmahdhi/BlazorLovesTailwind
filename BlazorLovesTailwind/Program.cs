ef07212fd0ee64d429a80d5e48394b7ba88589b6
Mabrouk Mahdhi <coding@mahdhi.com>
2025-01-24 17:16:53 +0100

    CONFIG: Install Tailwindcss

--- BlazorLovesTailwind/Program.cs
+++ BlazorLovesTailwind/Program.cs
@@ -18,7 +18,6 @@ namespace BlazorLovesTailwind
             if (!app.Environment.IsDevelopment())
             {
                 app.UseExceptionHandler("/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
 
